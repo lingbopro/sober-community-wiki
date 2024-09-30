99504ffca1d7661c77212cdc6df013c3d35c6898
lingbopro <ilovemc@zohomail.cn>
2024-09-30 21:56:05 +0800

    feat: i18next & sober

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
+        <s-page class="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
@@ -13,7 +13,7 @@ function App() {
                     Learn React
                 </a>
             </header>
-        </div>
+        </s-page>
     );
 }
 
